4571cb44842b4feea1f1eb032256119bc2cb6917
Kevin-Barbosa-Dev <kevin6252@outlook.com>
2025-04-09 21:34:53 -0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -4,9 +4,24 @@ import model.Grafo;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        Grafo grafo = new Grafo(3);
+       Grafo grafo = new Grafo(4);
 
-        grafo.adicionar(0,1,0);
-        grafo.adicionar();
+        // Adicionando vértices
+        grafo.adicionarVertice(0, "A");
+        grafo.adicionarVertice(1, "B");
+        grafo.adicionarVertice(2, "C");
+        grafo.adicionarVertice(3, "D");
+
+        // Adicionando arestas com distâncias
+        grafo.adicionarAresta(0, 1, 5); // A-B com distância 5
+        grafo.adicionarAresta(1, 2, 10); // B-C com distância 10
+        grafo.adicionarAresta(0, 2, 15); // A-C com distância 15
+        grafo.adicionarAresta(2, 3, 5); // C-D com distância 5
+
+
+        // Exibindo a matriz de adjacência
+        grafo.exibirMatrizAdjacente();
+
+        grafo.buscarVizinhos(2);
     }
 }
