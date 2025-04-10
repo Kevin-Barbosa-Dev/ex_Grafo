b86c606b66416a047e723b4a4fa335347a838f8a
Kevin-Barbosa-Dev <kevin6252@outlook.com>
2025-04-09 21:37:08 -0300

    Update Grafo.java

--- src/model/Grafo.java
+++ src/model/Grafo.java
@@ -4,28 +4,70 @@ import java.util.Scanner;
 
 public class Grafo {
 
-    private int[][] matriz;
+   private int[][] matrizAdjacente;
+    private String[] vertices;
+    private int tamanho;
 
+    // Construtor que inicializa a matriz de adjacência
+    public Grafo(int tamanho) {
+        this.tamanho = tamanho;
+        matrizAdjacente = new int[this.tamanho][this.tamanho];
+        vertices = new String[this.tamanho];
 
-    public  Grafo(int numeroDeVerteces) {
-        matriz = new int[numeroDeVerteces][numeroDeVerteces];
-
-
+        // Inicializa a matriz com -1 (indica que não há conexão entre os vértices)
+        for (int i = 0; i < this.tamanho; i++) {
+            for (int j = 0; j < this.tamanho; j++) {
+                matrizAdjacente[i][j] = -1;
+            }
+        }
     }
 
-    public void adicionar(int origem, int destino, int distancia){
-        matriz[origem][destino] = distancia;
-        matriz[destino][origem] = distancia;
+    // Método para adicionar um vértice ao grafo
+    public void adicionarVertice(int indice, String nome) {
+        if (indice >= 0 && indice < vertices.length) {
+            vertices[indice] = nome;
+        }
     }
 
-    public void exibir() {
+    // Método para adicionar uma aresta com uma distância entre dois vértices
+    public void adicionarAresta(int origem, int destino, int distancia) {
+        if (origem >= 0 && origem < matrizAdjacente.length && destino >= 0 && destino < matrizAdjacente.length) {
+            matrizAdjacente[origem][destino] = distancia;
+            matrizAdjacente[destino][origem] = distancia; // Para grafo não-direcionado
+        }
+    }
 
-        for (int i = 0; i < matriz.length; i++) {
-            for (int j = 0; j < matriz[i].length; j++) {
-                System.out.print(matriz[i][j] + " ");
+    // Método para exibir a matriz de adjacência
+    public void exibirMatrizAdjacente() {
+        System.out.println("Matriz de Adjacência:");
+        System.out.print("     ");
+        for (int i = 0; i < vertices.length; i++) {
+            System.out.print(vertices[i] + "   ");
+        }
+        System.out.println();
+
+        for (int i = 0; i < matrizAdjacente.length; i++) {
+            System.out.print(vertices[i] + " ");
+            for (int j = 0; j < matrizAdjacente[i].length; j++) {
+                if (matrizAdjacente[i][j] == -1) {
+                    System.out.print(" - ");
+                } else {
+                    System.out.print(matrizAdjacente[i][j] + "   ");
+                }
             }
             System.out.println();
         }
+    }
+
+    public void buscarVizinhos(int quantidadesDeVizinhos) {
+        int qunt = 0;
+        for (int i = 0; i < matrizAdjacente[quantidadesDeVizinhos].length; i++) {
+            if (matrizAdjacente[quantidadesDeVizinhos][i] != -1 && quantidadesDeVizinhos != i) {
+                qunt++;
+            }
+
+        }
+        System.out.println("O vértice " + vertices[quantidadesDeVizinhos] + " tem " + qunt + " vizinhos.");
     }
 
 
